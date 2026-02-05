301c3b7057922aae552739c17b1e71f0cbe7a2ec
yeonduhaeyo <yeonduhaeyo99@gmail.com>
2026-02-05 14:55:40 +0900

    ch05 Theme, CSS 적용

--- ch05/casting-defect-dashboard/app.py
+++ ch05/casting-defect-dashboard/app.py
@@ -1,11 +1,18 @@
 from shiny import App, ui
 from pages import page_predict, page_process, page_appendix
 
+import shinyswatch
+
+from shared import app_dir
+
 app_ui = ui.page_navbar(
+    ui.head_content(ui.include_css(app_dir / "www" / "style.css")),
+
     page_predict.page_predict_ui("predict"),
     page_process.page_process_ui("process"),
     page_appendix.page_appendix_ui("appendix"),
     title="주조공정 불량 예측 대시보드",
+    theme=shinyswatch.theme.flatly,
 )
 
 def server(input, output, session):
